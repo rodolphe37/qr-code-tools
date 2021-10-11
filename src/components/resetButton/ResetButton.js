06c0199971fc5880933cbe75c882e9d12ff62b8f
rodolphe37 <rodolphe.a@gmail.com>
2021-10-11 10:02:32 +0200

    added more styles & reset/reload button

--- src/components/resetButton/ResetButton.js
+++ src/components/resetButton/ResetButton.js
@@ -16,7 +16,7 @@ const ResetButton = () => {
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 18 18"
         >
-          <path fill="#777F7F" d="M9 0a9 9 0 1 1 0 18A9 9 0 0 1 9 0z" />
+          <path fill="#f50057" d="M9 0a9 9 0 1 1 0 18A9 9 0 0 1 9 0z" />
           <path
             fill="#FFF"
             d="M13.7 12.3l-1.4 1.4L9 10.42l-3.3 3.3-1.4-1.42L7.58 9l-3.3-3.3L5.7 4.3 9 7.58l3.3-3.3 1.4 1.42L10.42 9l3.3 3.3z"
